8363dacd874441b89fe3bd843932d724fb3a3763
迪奥布兰度日如年 <86916053+wohaocai555@users.noreply.github.com>
2026-05-18 04:13:00 +0800

    Add localization.lua

---
+++ localization.lua
@@ -0,0 +1,21 @@
+GMM.LOCALIZATION = {
+    gmm_title = "Give Me More",
+    gmm_joker = "Joker",
+    gmm_planet = "Planet",
+    gmm_tarot = "Tarot",
+    gmm_spectral = "Spectral",
+    gmm_money_btn = "Give Money",
+    gmm_select = "Select",
+    gmm_select_joker = "Select a Joker",
+    gmm_select_planet = "Select a Planet Card",
+    gmm_select_tarot = "Select a Tarot Card",
+    gmm_config_title = "Give Me More Settings",
+    gmm_negative = "Negative Edition on Jokers",
+    gmm_money = "Money Amount",
+}
+
+GMM.init_localization = function()
+    for k, v in pairs(GMM.LOCALIZATION) do
+        G.localization.misc.dictionary[k] = v
+    end
+end
